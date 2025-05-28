b4c6d741475d610ff88491c3c6e94dd334e512f5
Jonatan <sanchezjonatan21@gmail.com>
2025-05-27 22:55:07 -0600

    Adding Tailwind config for v4 and changing tab icon

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,24 +4,11 @@ export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        feijoa: {
-          50: "#f4f8ed",
-          100: "#e6f0d7",
-          200: "#cee2b4",
-          300: "#b3d28e",
-          400: "#90bb60",
-          500: "#739f43",
-          600: "#587e32",
-          700: "#44612a",
-          800: "#394e26",
-          900: "#324423",
-          950: "#18240f",
-        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
